ac929e8d6c4e6be50bf6ad0a8c4331c592673fef
dilithjay <dilithjay@gmail.com>
2022-11-02 16:56:26 +0530

    Fix condition order in if-else statement

--- img_styler/ui/components.py
+++ img_styler/ui/components.py
@@ -201,7 +201,36 @@ def get_controls(q: Q):
             value=q.client.yaw if q.client.yaw else 0,
         ),
     ]
-    if q.client.task_choice == 'B':
+    if q.client.task_choice == 'A': # Fix Resolution
+        img_name_parts = q.client.source_face.split('/')[-1].split('.')
+        new_img_name = '.'.join(img_name_parts[: -1]) + '_fixed.' + img_name_parts[-1]
+        disabled = q.client.processedimg is None
+        return ui.form_card(
+            box=ui.box(zone='side_controls', order=1),
+            items=[
+                task_choice_dropdown,
+                ui.dropdown(
+                    name='source_face',
+                    label='Source Face',
+                    choices=[
+                        ui.choice(name=x, label=os.path.basename(x))
+                        for x in q.app.source_faces
+                    ],
+                    value=q.client.source_face,
+                    tooltip='Select a source face to be enhanced.',
+                    trigger=True,
+                ),
+                ui.buttons([
+                    ui.button('fix_resolution', 'Fix Resolution', primary=True)
+                ], justify='end'),
+                ui.separator(),
+                ui.textbox('img_name', 'Add fixed image to list', value=new_img_name, disabled=disabled),
+                ui.buttons([
+                    ui.button('save_img_to_list', 'Add', primary=True, disabled=disabled)
+                ], justify='end'),
+            ]
+        )
+    elif q.client.task_choice == 'B':
         # Includes styling controls.
         return ui.form_card(
             box=ui.box(
@@ -345,35 +374,6 @@ def get_controls(q: Q):
             for _index, _item in enumerate(landmark_controls)
         ]
         return edit_controls
-    elif q.client.task_choice == 'A': # Fix Resolution
-        img_name_parts = q.client.source_face.split('/')[-1].split('.')
-        new_img_name = '.'.join(img_name_parts[: -1]) + '_fixed.' + img_name_parts[-1]
-        disabled = q.client.processedimg is None
-        return ui.form_card(
-            box=ui.box(zone='side_controls', order=1),
-            items=[
-                task_choice_dropdown,
-                ui.dropdown(
-                    name='source_face',
-                    label='Source Face',
-                    choices=[
-                        ui.choice(name=x, label=os.path.basename(x))
-                        for x in q.app.source_faces
-                    ],
-                    value=q.client.source_face,
-                    tooltip='Select a source face to be enhanced.',
-                    trigger=True,
-                ),
-                ui.buttons([
-                    ui.button('fix_resolution', 'Fix Resolution', primary=True)
-                ], justify='end'),
-                ui.separator(),
-                ui.textbox('img_name', 'Add fixed image to list', value=new_img_name, disabled=disabled),
-                ui.buttons([
-                    ui.button('save_img_to_list', 'Add', primary=True, disabled=disabled)
-                ], justify='end'),
-            ]
-        )
     else: # Option: 'D' Image Prompt
         return ui.form_card(box=ui.box(
                 zone='side_controls',
