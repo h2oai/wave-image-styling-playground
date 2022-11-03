26c35e5fb82d52a3941e7c4e2074fb0b803cfacd
pramitchoudhary <pramitchoudhary@gmail.com>
2022-11-02 18:20:52 -0700

    Use K-LMS as the default scheduler(sampler)

--- img_styler/image_prompt/stable_diffusion.py
+++ img_styler/image_prompt/stable_diffusion.py
@@ -3,7 +3,7 @@ from io import BytesIO
 from typing import Optional
 
 import torch
-from diffusers import StableDiffusionImg2ImgPipeline, StableDiffusionPipeline
+from diffusers import LMSDiscreteScheduler, StableDiffusionImg2ImgPipeline, StableDiffusionPipeline
 from PIL import Image
 from torch import autocast
 
@@ -15,21 +15,33 @@ def generate_image_with_prompt(input_img_path: Optional[str]=None, prompt_txt: s
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model_path = "./models/stable_diffusion_v1_4"
 
+    # Default Scheduler K-LMS(Katherine Crowson)
+    # TODO Enable ability to switch different Schedulers
+    lms = LMSDiscreteScheduler(
+        beta_start=0.00085,
+        beta_end=0.012,
+        beta_schedule="scaled_linear"
+    )
     if input_img_path:
         pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model_path, revision="fp16",
                                                                     torch_dtype=torch.float16).to(device)
+        pipe.scheduler = lms
         # Open image
         image_input = Image.open(input_img_path).convert("RGB")
         init_image = image_input.resize((512, 512))
 
         with autocast(device):
-            images = pipe(prompt=prompt_txt, init_image=init_image, strength=0.5, guidance_scale=7.5)["sample"]
+            images = pipe(prompt=prompt_txt, init_image=init_image, strength=0.5, guidance_scale=7.5,
+                                                                        num_inference_steps=50)["sample"]
     else: # Default prompt
+        generator = torch.Generator(device=device).manual_seed(42)
         pipe = StableDiffusionPipeline.from_pretrained(model_path, revision="fp16",
                                                                     torch_dtype=torch.float16).to(device)
-
+        pipe.scheduler = lms
         with autocast(device):
-            images = pipe(prompt=prompt_txt).images
+            # One sample for now.
+            # TODO Extend for multiple samples.
+            images = pipe(prompt=[prompt_txt]*1, num_inference_steps=50, generator=generator).images
 
 
     file_name = output_path + '/result.jpg'
